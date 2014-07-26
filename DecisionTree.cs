2dc4de3d24c7573d9c8d8eaee1cf8b9c6954ff01
Uncle.Chao <uncle.chao@live.cn>
2014-07-26 10:51:48 +0800

    Definition of tree

---
+++ DecisionTree.cs
@@ -0,0 +1,23 @@
+﻿//==================================================
+//Copyright (C) 2013
+//File Name   :   Program.cs
+//Author  :   ZhangChao
+//Created Date    :   2013-6-20
+//Instruction    :  决策树定义
+//Revision History    :
+//       
+//==================================================
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 决策树
+{
+    public class DecisionTree
+    {
+        public string Name;
+        public Dictionary<string, DecisionTree> Next;
+    }
+}
